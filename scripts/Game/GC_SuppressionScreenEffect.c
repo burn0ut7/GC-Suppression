99d1d27afb4a41e62620e7d9ddd4b4b7093531db
burn0ut7 <46172991+burn0ut7@users.noreply.github.com>
2026-01-10 12:27:14 -0500

    tweaks

--- scripts/Game/GC_SuppressionScreenEffect.c
+++ scripts/Game/GC_SuppressionScreenEffect.c
@@ -3,13 +3,13 @@ class GC_SuppressionScreenEffect : SCR_BaseScreenEffect
 	[Attribute("1.0", UIWidgets.Auto, "Multiplier applied to vignette mask")]
 	protected float m_fVignetteMultiplier;
 	
-	[Attribute("0.6", UIWidgets.Auto, "Maxium mask for vignette", params: "0 1")]
+	[Attribute("0.6", UIWidgets.Auto, "Maximum mask for vignette", params: "0 1")]
 	protected float m_fVignetteMax;
 	
 	[Attribute("1.0", UIWidgets.Auto, "Multiplier applied to blur intensivty")]
 	protected float m_fBlurMultiplier;
 	
-	[Attribute("0.2", UIWidgets.Auto, "Maxium blur intensivty", params: "0 0.2")]
+	[Attribute("0.2", UIWidgets.Auto, "Maximum blur intensivty", params: "0 0.2")]
 	protected float m_fBlurMax;
 	
 	[Attribute("1.0", UIWidgets.Auto, "Multiplier applied to blur size")]
@@ -18,17 +18,23 @@ class GC_SuppressionScreenEffect : SCR_BaseScreenEffect
 	[Attribute("0", UIWidgets.Auto, "Minimum blur size", params: "0 0.5")]
 	protected float m_fBlurSizeMin;
 
-	[Attribute("0", UIWidgets.Auto, "Minimum saturation", params: "0 1")]
+	[Attribute("0.25", UIWidgets.Auto, "Minimum saturation", params: "0 1")]
 	protected float m_fSaturationMin;
 	
 	[Attribute("1.0", UIWidgets.Auto, "Multiplier applied to saturation")]
 	protected float m_fSaturationMultiplier;
 	
-	[Attribute("1.25", UIWidgets.Auto, "Minimum contrast", params: "1 2")]
+	[Attribute("1.1", UIWidgets.Auto, "Minimum contrast", params: "1 2")]
 	protected float m_fContrastMax;
 	
 	[Attribute("1.0", UIWidgets.Auto, "Multiplier applied to blur size")]
 	protected float m_fContrastMultiplier;
+	
+	[Attribute("0.01", UIWidgets.Auto, "Maximum Chromatic Aberration", params: "0 0.05")]
+	protected float m_fChromAberMax;
+	
+	[Attribute("1.0", UIWidgets.Auto, "Multiplier applied to Chromatic Aberration")]
+	protected float m_fChromAberMultiplier;
 
 	protected ImageWidget m_wVignette;
 	protected ImageWidget m_wFlinch;
@@ -39,7 +45,7 @@ class GC_SuppressionScreenEffect : SCR_BaseScreenEffect
 	protected static float s_fBlurrinessSize = 0.5;
 	
 	protected const string m_sRadialBlurMaterialName = "{E0162CBB3FA4AC16}UI/Materials/GC_ScreenEffects_BlurPP.emat";
-	protected const int m_sRadialBlurPriority = 18;
+	protected const int m_sRadialBlurPriority = 17;
 	
 	//Color
 	protected static bool s_bEnableColorEffect = false;
@@ -47,7 +53,15 @@ class GC_SuppressionScreenEffect : SCR_BaseScreenEffect
 	protected static float s_fSaturation = 1;
 	
 	protected const string m_sColorEffectMaterialName = "{C7CAE196FEBD4469}UI/Materials/GC_ScreenEffects_ColorPP.emat";
-	protected const int m_sColorEffectPriority = 19;
+	protected const int m_sColorEffectPriority = 18;
+	
+	//Chrom aber
+	protected static bool s_bEnableChromAberEffect = false;
+	protected static float s_fChromAber = 0;
+	
+	protected const string m_sChromAberMaterialName = "{07258569ACAB792D}UI/Materials/GC_ScreenEffects_ChromAberPP.emat";
+	protected const int m_sChromAberPriority = 19;
+	
 	
 	override void DisplayStartDraw(IEntity owner)
 	{
@@ -59,6 +73,7 @@ class GC_SuppressionScreenEffect : SCR_BaseScreenEffect
 
 		GetGame().GetWorld().SetCameraPostProcessEffect(CameraId, m_sRadialBlurPriority, PostProcessEffectType.RadialBlur, m_sRadialBlurMaterialName);
 		GetGame().GetWorld().SetCameraPostProcessEffect(CameraId, m_sColorEffectPriority, PostProcessEffectType.Colors, m_sColorEffectMaterialName);
+		GetGame().GetWorld().SetCameraPostProcessEffect(CameraId, m_sChromAberPriority, PostProcessEffectType.ChromAber, m_sChromAberMaterialName);
 	}
 	
 	void Flinch()
@@ -88,20 +103,20 @@ class GC_SuppressionScreenEffect : SCR_BaseScreenEffect
 	void UpdateSuppresion()
 	{
 		float suppressionAmount = GC_SuppressionSystem.GetInstance().GetAmount();
-		
+
+		//Vignette
 		float maskProgress = Math.Clamp(suppressionAmount * m_fVignetteMultiplier * m_fVignetteMax, 0, m_fVignetteMax);
 		m_wVignette.SetMaskProgress(maskProgress);
 		
+		// Blur
 		s_fBlurriness = Math.Clamp(suppressionAmount * m_fBlurMultiplier * m_fBlurMax, 0, m_fBlurMax);
 
 		float blurSizeT = suppressionAmount * m_fBlurSizeMultiplier;
 		blurSizeT = Math.Clamp(blurSizeT, 0, 1);
 		s_fBlurrinessSize = Math.Lerp(0.5, m_fBlurSizeMin, blurSizeT);
 		
-		// -0.1 to give 10% buffer to saturation so it doesn't apply instantly from 1 shot
-		float satT = (suppressionAmount - 0.1) / 0.9;
-		satT = Math.Clamp(satT, 0, 1);
-		satT *= m_fSaturationMultiplier;
+		//Color
+		float satT = suppressionAmount * m_fContrastMultiplier;
 		satT = Math.Clamp(satT, 0, 1);
 		s_fSaturation = Math.Lerp(1.0, m_fSaturationMin, satT);
 		
@@ -109,15 +124,20 @@ class GC_SuppressionScreenEffect : SCR_BaseScreenEffect
 		conT = Math.Clamp(conT, 0, 1);
 		s_fContrast = Math.Lerp(1.0, m_fContrastMax, conT);
 
+		//Chrom Aber
+		s_fChromAber = Math.Clamp(suppressionAmount * m_fChromAberMultiplier * m_fChromAberMax, 0, m_fChromAberMax);
+		
 		if(suppressionAmount <= 0)
 		{
 			s_bEnableRadialBlur = false;
 			s_bEnableColorEffect = false;
+			s_bEnableChromAberEffect = false;
 		}
 		else
 		{
 			s_bEnableRadialBlur = true;
 			s_bEnableColorEffect = true;
+			s_bEnableChromAberEffect = true;
 		}
 	}
 	
@@ -128,12 +148,15 @@ class GC_SuppressionScreenEffect : SCR_BaseScreenEffect
 		
 		s_bEnableRadialBlur = false;
 		s_bEnableColorEffect = false;
+		s_bEnableChromAberEffect = false;
 		
 		s_fBlurriness = 0;
 		s_fBlurrinessSize = 0.5;
 		
 		s_fContrast = 1;
 		s_fSaturation = 1;
+		
+		s_fChromAber = 0;
 		
 		ClearFlinch();
 	}
