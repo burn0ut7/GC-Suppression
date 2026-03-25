74d7c3a00e2354fa7422f792f0a9fecd638fd904
burn0ut7 <46172991+burn0ut7@users.noreply.github.com>
2026-03-25 04:39:32 -0400

    Create GC_PS_LOBBY.c

---
+++ scripts/Game/compatibility/GC_PS_LOBBY.c
@@ -0,0 +1,23 @@
+#ifdef PS_LOBBY
+modded class GC_SuppressionSystem
+{
+	override void RegisterProjectile(IEntity projectile)
+	{
+		Print("GC PS_Lobby PATCH");
+		PS_PlayableControllerComponent playable = PS_PlayableControllerComponent.GetInstance();
+		if(playable.IsSpectating())
+			return;
+		
+		super.RegisterProjectile(projectile);
+	}
+	
+	override protected void AddSuppression(float suppression)
+	{
+		PS_PlayableControllerComponent playable = PS_PlayableControllerComponent.GetInstance();
+		if(playable.IsSpectating())
+			return;
+		
+		super.AddSuppression(suppression);
+	}
+}
+#endif
